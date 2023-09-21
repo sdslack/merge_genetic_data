7d051fa7370d8acb79b266688811cce21116df49
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-09-21 10:16:14 -0600

    Add output names, allele order, switch to indep-pairwise

--- plink_genome.sh
+++ plink_genome.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 plink_bed_file=$1
-window_size=$2
-step_size=$3
-vif_threshold=$4
+window_size=$2  # window size in kb, e.g. 10000
+step_size=$3  # e.g. 5
+r2=$4  # e.g. 0.1
 
 plink_in_file=`echo $plink_bed_file | sed 's/.bed//'`
 
 plink --bfile $plink_in_file \
-   --indep $window_size $step_size $vif_threshold 
+   --keep-allele-order \
+   --indep-pairwise ${window_size}['kb'] $step_size $r2 \
+   --out ${plink_in_file}_to_prune
+
 plink --bfile $plink_in_file \
-   --extract plink.prune.in \
+   --extract ${plink_in_file}_to_prune.prune.in \
+   --keep-allele-order \
    --make-bed --out ${plink_in_file}_pruned
 
 plink --bfile ${plink_in_file}_pruned \
-   --genome 
+   --keep-allele-order \
+   --genome \
+   --out ${plink_in_file}_pruned_ibd
