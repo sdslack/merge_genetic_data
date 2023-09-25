53f58dc20ffab84d8bd3dfa50a3baa41406f6a87
sdslack <sarah.slack@cuanschutz.edu>
2023-09-25 12:25:28 -0600

    Add output prefix as arg

--- convert_vcf_to_plink.sh
+++ convert_vcf_to_plink.sh
@@ -2,16 +2,16 @@
 
 if [ "$#" -eq 0 ]
 then
-   echo "Usage: ${0##*/} <input_vcf>"
+   echo "Usage: ${0##*/} <input_vcf> <output_prefix>"
    echo "Converts VCF to PLINK1.9 file."
    exit
 fi
 
 input_vcf=$1
-input_vcf_name=$(basename $input_vcf | sed 's/\.vcf.*//')
+output_prefix=$2
 
 plink --vcf "$input_vcf" \
         --keep-allele-order \
         --make-bed --double-id \
-        --out "$input_vcf_name"
+        --out "$output_prefix"
 
