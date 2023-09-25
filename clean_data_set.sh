9d49bf2732afab8aeb605030b6234eb2809a3723
sdslack <sarah.slack@cuanschutz.edu>
2023-09-25 10:57:11 -0600

    Add keep-allele-order

--- clean_data_set.sh
+++ clean_data_set.sh
@@ -6,6 +6,7 @@ plink_prefix=$2
 #Remove all non-autosomal SNPs
 plink --bfile $plink_in_file \
    --autosome \
+   --keep-allele-order \
    --make-bed --out ${plink_prefix}_autosome
 
 #Remove all strand ambiguous SNPs
@@ -15,24 +16,28 @@ cat ${plink_prefix}_autosome.bim | awk '{print $2"\t"$5 $6}' | grep CG$ | cut -f
 cat ${plink_prefix}_autosome.bim | awk '{print $2"\t"$5 $6}' | grep GC$ | cut -f1 >> ${plink_prefix}_strand_amb_variants.txt
 plink --bfile ${plink_prefix}_autosome \
    --exclude ${plink_prefix}_strand_amb_variants.txt \
+   --keep-allele-order \
    --make-bed --out ${plink_prefix}_autosome_no_strand_amb
 
 #Remove all variants with duplicate IDs
 cat ${plink_prefix}_autosome_no_strand_amb.bim | awk '{print $2}' | sort | uniq -d > ${plink_prefix}_dupl_snp_ids.txt
 plink --bfile ${plink_prefix}_autosome_no_strand_amb \
    --exclude ${plink_prefix}_dupl_snp_ids.txt \
+   --keep-allele-order \
    --make-bed --out ${plink_prefix}_autosome_no_strand_amb_no_dupl_snp_ids
 
 #Update PLINK SNP names as chr:position
 cat ${plink_prefix}_autosome_no_strand_amb_no_dupl_snp_ids.bim | awk '{print $2"\t"$1":"$4}' > ${plink_prefix}_new_snp_ids.txt
 plink --bfile ${plink_prefix}_autosome_no_strand_amb_no_dupl_snp_ids \
    --update-map ${plink_prefix}_new_snp_ids.txt --update-name \
+   --keep-allele-order \
    --make-bed --out ${plink_prefix}_autosome_no_strand_amb_no_dupl_snp_ids_chr_pos_snp_ids
 
 #Remove all variants with duplicate positions
 cat ${plink_prefix}_autosome_no_strand_amb_no_dupl_snp_ids_chr_pos_snp_ids.bim | awk '{print $2}' | sort | uniq -d > ${plink_prefix}_dupl_pos_snps.txt
 plink --bfile ${plink_prefix}_autosome_no_strand_amb_no_dupl_snp_ids_chr_pos_snp_ids \
    --exclude ${plink_prefix}_dupl_pos_snps.txt \
+   --keep-allele-order \
    --make-bed --out ${plink_prefix}_clean
 
 
