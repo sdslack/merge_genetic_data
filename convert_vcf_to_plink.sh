0eb9f5f82d1d3f5441705381d08d3563ffef6352
sdslack <sarah.slack@cuanschutz.edu>
2023-09-25 10:52:11 -0600

    Add keep allele order, bash syntax

--- convert_vcf_to_plink.sh
+++ convert_vcf_to_plink.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-in_vcf=$1
-out_plink_prefix=$2
+if [ "$#" -eq 0 ]
+then
+   echo "Usage: ${0##*/} <input_vcf>"
+   echo "Converts VCF to PLINK1.9 file."
+   exit
+fi
 
-plink --vcf $in_vcf --make-bed --out $out_plink_prefix
+input_vcf=$1
+input_vcf_name=$(basename $input_vcf | sed 's/\.vcf.*//')
+
+plink --vcf "$input_vcf" \
+        --keep-allele-order \
+        --make-bed --double-id \
+        --out "$input_vcf_name"
+
